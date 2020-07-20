fixing CJAM bug, rotation bug (?)

mcmc_dynamics/analysis/cjam/axisymmetric.py
@@ -163,9 +163,9 @@ class Axisymmetric(Runner):
     def lnlike(self, values, return_model=False):
         x = np.copy(self.x)
         y = np.copy(self.y)
-
+        
         current_parameters = self.fetch_parameters(values)
-
+        
         unique_id = uuid.uuid4()
 
         with printoptions(precision=3):
@@ -183,7 +183,7 @@ class Axisymmetric(Runner):
 
         x -= current_parameters['delta_x']
         y -= current_parameters['delta_y']
-
+        
         # rotating data to determine rotation angle of cluster
         # copied from data_reader.DataReader.rotate()
         theta0 = np.arctan2(current_parameters['kappa_y'], current_parameters['kappa_x'])
@@ -191,8 +191,25 @@ class Axisymmetric(Runner):
         #self.x = self.x * np.cos(theta0) + self.y * np.sin(theta0)
         #self.y = -self.x * np.sin(theta0) + self.y * np.cos(theta0)
         
-        x = x * np.cos(theta0) + y * np.sin(theta0)
-        y = -x * np.sin(theta0) + y * np.cos(theta0)
+        xnew = x * np.cos(theta0) + y * np.sin(theta0)
+        ynew = -x * np.sin(theta0) + y * np.cos(theta0)
+        
+        x = xnew
+        y = ynew        
+
+        # fixing cjam bug where it throws nans for star too close to centre
+
+        xa = x.to(u.arcmin).value
+        ya = y.to(u.arcmin).value
+                
+        xa = np.where((xa < 1e-3) & (xa > 0), 1e-3, xa)
+        xa = np.where((xa > -1e-3) & (xa < 0), -1e-3, xa)
+        
+        ya = np.where((ya < 1e-3) & (ya > 0), 1e-3, ya)
+        ya = np.where((ya > -1e-3) & (ya < 0), -1e-3, ya)        
+        
+        x = xa * u.arcmin
+        y = ya * u.arcmin
         
         self.v += current_parameters['delta_v']
 
@@ -203,7 +220,8 @@ class Axisymmetric(Runner):
                                       mscale=current_parameters['mlr'], incl=incl, mbh=current_parameters['mbh'],
                                       rbh=current_parameters['rbh'])
 
-        except ValueError:
+        except ValueError as err:
+            logging.warn("CJAM returned an error:", err)
             return -np.inf
 
         logger.debug('CJAM call succeeded for {0}.'.format(unique_id))
@@ -214,7 +232,9 @@ class Axisymmetric(Runner):
 
         # calculate likelihood
         if not (v2zz > vz**2).all():
+            logging.error("Strange velocities or nan velocities.")
             return -np.inf
+
         if return_model:
             lnlike = self._calculate_lnlike(v_los=vz, sigma_los=np.sqrt(v2zz - vz**2))
             return lnlike, x, y, vz, v2zz